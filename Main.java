524ba6ad498f405717db09884fcdf95cf77de741
ay a <doydoy.com.r@gmail.com>
2021-03-26 03:42:49 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+class Main {
+  public static void main(String[] args) {
+    double num1 = 4.5;
+    double num2 = 4.5;
+		double ADDIT1 = 0.5;
+		double ADDIT2 = 0.5;
+		double sum1 = num1 + num2;
+		double sum2 = ADDIT1 + ADDIT2;
+		double fsum = sum1 + sum2;
+		System.out.println(fsum);
+  }
+}
